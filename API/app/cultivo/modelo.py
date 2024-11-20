e0d3137fa0f66828625bd0b57aed522cd202b3fa
Brayan Alejandro Herrera Amariles <braherre@bancolombia.com.co>
2024-11-19 20:05:54 -0500

    WIP

--- API/app/cultivo/modelo.py
+++ API/app/cultivo/modelo.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import relationship
 
 from .. import db
-from ..tournament.modelo import Tournament
+
+from ..evento.modelo import Evento
 
 
 class Cultivo(db.Base):
@@ -14,14 +16,27 @@ class Cultivo(db.Base):
     variedad = Column("variedad", String(200), nullable=False)
     ubicacion = Column("ubicacion", String(200), nullable=False)
     tamano = Column("tamano", String(200), nullable=False)
+    fecha_siembra = Column("fecha_siembra", Date, nullable=False)
     fecha_cosecha = Column("fecha_cosecha", Date, nullable=False)
-    tournament = relationship(Tournament)
+    tournament = relationship(Evento)
 
 
 class CultivoIn(BaseModel):
     name: str
+    tipo: str
+    variedad: str
+    ubicacion: str
+    tamano: str
+    fecha_siembra: datetime
+    fecha_cosecha: datetime
 
 
 class CultivoOut(BaseModel):
     id: int = 1
-    name: str = "League of Legends"
+    name: str = "Lentejas"
+    tipo: str = "Legumbre"
+    variedad: str = "Española"
+    ubicacion: str = "123;456"
+    tamano: str = "25"
+    fecha_siembra: datetime = datetime.now()
+    fecha_cosecha: datetime = datetime.now()
---
+++ API/app/evento/modelo.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+from sqlalchemy import Column, Integer, String, Date, Enum
+from sqlalchemy.sql.schema import ForeignKey
+from enum import Enum as PyEnum
+
+from .. import db
+
+class NivelEnum(PyEnum):
+    CRITICO = "critico"
+    ALTO = "alto"
+    MEDIO = "medio"
+    BAJO = "bajo"
+
+class Evento(db.Base):
+    __tablename__ = "evento"
+    id = Column("id", Integer, autoincrement=True, primary_key=True, unique=True)
+    titulo = Column("titulo", String(200), nullable=False)
+    descripcion = Column("descripcion", String(400), nullable=False)
+    nivel = Column("nivel", Enum(NivelEnum), nullable=False)
+    fecha_evento = Column("fecha_evento", Date, nullable=False)
+
+    cultivo_id = Column(Integer, ForeignKey("cultivo.id"))
+
+
+class EventoIn(BaseModel):
+    titulo: str
+    descripcion: NivelEnum = Field(..., description="Nivel del evento: critico, alto, medio, bajo")
+    nivel: str
+    fecha_evento: datetime
+
+    cultivo_id: int
+
+
+class EventoOut(BaseModel):
+    id: str = 1
+    titulo: str = "Plaga detectada"
+    descripcion: str = "Una plaga se a detectado en tu zona"
+    nivel: NivelEnum = NivelEnum.CRITICO
+    fecha_evento: datetime = datetime.now()
+
+    cultivo_id: int = 10
