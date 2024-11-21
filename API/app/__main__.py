9754bff51ea60188f60fa7558bddc0bd58875cc9
bramenn <bramendev@gmail.com>
2024-11-20 23:00:42 -0500

    Ejecutar todo cada pr

--- API/app/__main__.py
+++ API/app/__main__.py
@@ -11,4 +11,4 @@ app.include_router(cultivo_endpoint.router, prefix="/v1/cultivo", tags=["cultivo
 
 if __name__ == "__main__":
     Base.metadata.create_all(conn)
-    uvicorn.run(app=app, host="0.0.0.0", port=5000)
+    uvicorn.run(app=app, host="0.0.0.0", port=80)
